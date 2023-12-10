84ec16b19e3c4164a9bdbbb89742d3717d16f6dc
Atanas Radoynov <radoinov98@gmail.com>
2023-12-10 12:18:21 +0200

    Homework
    Upoading homework solutions(Unit testing objects and classes)

---
+++ ExerciseUnitTestingObjectsAndClasses/TestApp.UnitTests/ArticleTests.cs
@@ -0,0 +1,89 @@
+﻿using NUnit.Framework;
+
+using System;
+using System.Collections.Generic;
+
+namespace TestApp.UnitTests;
+
+public class ArticleTests
+{
+    private Article _article;
+
+    [SetUp]
+    public void SetUp()
+    {
+        this._article = new Article();
+    }
+
+    [Test]
+    public void Test_AddArticles_ReturnsArticleWithCorrectData()
+    {
+        // Arrange
+        string[] articleData = 
+        {
+            "Article Content Author", 
+            "Article2 Content2 Author2", 
+            "Article3 Content3 Author3",
+        };
+
+        // Act
+        Article result = _article.AddArticles(articleData);
+
+        // Assert
+        Assert.That(result.ArticleList, Has.Count.EqualTo(3));
+        Assert.That(result.ArticleList[0].Title, Is.EqualTo("Article"));
+        Assert.That(result.ArticleList[1].Content, Is.EqualTo("Content2"));
+        Assert.That(result.ArticleList[2].Author, Is.EqualTo("Author3"));
+    }
+
+    [Test]
+    public void Test_GetArticleList_SortsArticlesByTitle()
+    {
+        // Arrange
+        Article inputArticle = new Article();
+        {
+
+                new Article()
+                {
+
+                    Title = "Article123",
+                    Content = "Content",
+                    Author = "Author"
+                };
+                new Article()
+                {
+                    Title = "Article12",
+                    Content = "Content2",
+                    Author = "Author2"
+                };
+                new Article()
+                {
+                    Title = "Article1",
+                    Content = "Content3",
+                    Author = "Author3"
+                };
+            };
+            
+        
+
+        string expected =
+        
+            $"Article1 - Content: Author{Environment.NewLine}" +
+            $"Article12 - Content2: Author2{Environment.NewLine}" +
+            $"Article123 - Content3: Author3";
+
+        // Act
+        string actual = _article.GetArticleList(inputArticle, "title");
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+
+    }
+
+
+    [Test]
+    public void Test_GetArticleList_ReturnsEmptyString_WhenInvalidPrintCriteria()
+    {
+        // TODO: finish test
+    }
+}
